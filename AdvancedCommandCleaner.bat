79f550fca59328c206b359ab393cca300e2a9dba
Vichingo455 <59311016+Vichingo455@users.noreply.github.com>
2022-01-14 21:26:18 +0100

    Add files via upload

---
+++ AdvancedCommandCleaner.bat
@@ -0,0 +1,56 @@
+@echo off
+clr
+color a
+title Advanced Command Cleaner
+echo ---------------------------------------
+echo Advanced Command Cleaner
+echo Before using Advanced Command Cleaner, make sure read ReadMe.txt
+systeminfo | findstr /B /C:"OS Name"
+echo ---------------------------------------
+
+echo Processing...
+
+:Stage1 - CheckDisk
+clr
+echo ---------------------------------------
+echo Advanced Command Cleaner
+echo ---------------------------------------
+echo
+echo Stage 1 - Checking disk...
+echo ---------------------------------------
+chkdsk c: /f
+echo ---------------------------------------
+
+:Stage2 - RepairDisk
+clr
+echo ---------------------------------------
+echo Advanced Command Cleaner
+echo ---------------------------------------
+echo
+echo Stage 2 - Repairing disk...
+echo ---------------------------------------
+echo sfc /scannow
+echo ---------------------------------------
+
+:Stage3 - CleanDisk
+clr
+echo ---------------------------------------
+echo Advanced Command Cleaner
+echo ---------------------------------------
+echo 
+echo Stage 3 - Cleaning disk...
+echo ---------------------------------------
+echo cleanmgr /d c:
+echo ---------------------------------------
+
+:Stage4 - RestartPC
+clr
+echo ---------------------------------------
+echo Advanced Command Cleaner
+echo ---------------------------------------
+echo
+echo Stage 4 - Restarting PC...
+echo ---------------------------------------
+echo Restarting... ---------------------------------------
+timeout 5 > NUL
+wmic os where primary=1 reboot
